dcd74fb92242e954e1dfd8ea34b747cd06f3d9b9
Charles Gustav <charlygustav@users.noreply.github.com>
2026-05-27 00:14:15 -0400

    chore(vc): add Google public STUN servers for maximum WebRTC reliability

--- voice-channel-widget.js
+++ voice-channel-widget.js
@@ -359,6 +359,8 @@
 
       this.ice = {
         iceServers: [
+          { urls: 'stun:stun.l.google.com:19302' },
+          { urls: 'stun:stun1.l.google.com:19302' },
           { urls: 'stun:stun.relay.metered.ca:80' },
           {
             urls: 'turn:global.relay.metered.ca:80',
